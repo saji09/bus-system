509cdb3e6c1a5d87a08c2454053aafebca3ac70c
saji09 <sajipragas20001109ss@gmail.com>
2023-12-08 14:49:54 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
             <Routes>
             <Route path="/login" element={<Login />} />
 
-            <Route path="/home" element={<Home />} />
+            <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/booking" element={<BusSeatBooking />} />
             <Route path="/bus-list" element={<BusList />} />
